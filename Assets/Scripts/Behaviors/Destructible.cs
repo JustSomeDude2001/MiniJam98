0c2c4b209765ba5a75c75b84fd1741a41302d7fb
JustSomeDude2001 <kim.yaroslavik@gmail.com>
2022-01-22 10:51:26 +0300

    Basic agressive behavior

---
+++ Assets/Scripts/Behaviors/Destructible.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    private int healthCurrent = 1;
+
+    /// <summary>
+    /// Maximum health can be safely modified as needed - clamping to
+    /// prevent immortality or the like is done in methods concerning
+    /// current health.
+    /// </summary>
+    public int healthMax = 1;
+
+    private void Start() {
+        healthCurrent = healthMax;
+    }
+
+    public int GetHealth() {
+        return healthCurrent;
+    }
+
+    /// <summary>
+    /// Used to prevent health overflow and track when to kill object.
+    /// </summary>
+    private void ClampHealth() {
+        if (healthCurrent > healthMax) {
+            healthCurrent = healthMax;
+        }
+        if (healthCurrent <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage) {
+        ClampHealth();
+        healthCurrent -= damage;
+        ClampHealth();
+    }
+
+    public void Heal(int value) {
+        ClampHealth();
+        healthCurrent += value;
+        ClampHealth();
+    }
+}
