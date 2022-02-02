6f3ce4665b7f341ef014bc1d051766d4c124d46d
relisqu <kopkopkop47@gmail.com>
2022-02-02 11:53:05 +0300

    twicking animations

--- Assets/Scripts/Behaviors/Game/Movable.cs
+++ Assets/Scripts/Behaviors/Game/Movable.cs
@@ -9,38 +9,45 @@ public class Movable : MonoBehaviour
 {
     public string modifierName;
     public Vector2 direction = Vector2.zero;
-    
+
     public float speed = 1;
 
     Rigidbody2D selfRigidBody;
     Animator selfAnimator;
     [SerializeField] private ParticleSystem Dust;
 
-    private void Start() {
+    private void Start()
+    {
         selfRigidBody = GetComponent<Rigidbody2D>();
         selfAnimator = GetComponent<Animator>();
     }
 
-    public float GetSpeed() {
+    public float GetSpeed()
+    {
         return speed * Player.GetModifier(modifierName) * Player.GetModifier(Player.ToTemp(modifierName));
     }
 
-    private void Update() {
+    private void Update()
+    {
         float currentSpeed = GetSpeed();
-        Vector3 newVelocity = new Vector3(direction.x, direction.y, 0) * currentSpeed;
+        var newVelocity = direction * currentSpeed;
         selfRigidBody.velocity = newVelocity;
         if (tag == "Player")
         {
-            selfAnimator.SetBool("isIdle", !(newVelocity.magnitude > 0));
-            if (direction.magnitude > 0.1)
+            selfAnimator.SetBool("isIdle", !(newVelocity.sqrMagnitude > 0));
+            if (newVelocity.sqrMagnitude > 0)
             {
-                Dust.Play();
+                if (_previousVelocity == Vector2.zero)
+                    Dust.Play();
             }
             else
             {
                 Dust.Stop();
             }
         }
-        
+
+        _previousVelocity = newVelocity;
     }
+
+    private Vector2 _previousVelocity = Vector2.zero;
 }
