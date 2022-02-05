3fa2f5d45810231c59faa77b2d0b992046fec16d
JustSomeDude2001 <kim.yaroslavik@gmail.com>
2022-02-05 20:01:31 +0300

    Temp upgrades now appear when unlocked

---
+++ Assets/Scripts/Behaviors/UI/TempCostDisplay.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TempCostDisplay : MonoBehaviour
+{
+    public TempUpgrade tempUpgrade;
+    public TextMeshProUGUI text;
+    public string maxedOutText;
+
+    private void Update() {
+        if (tempUpgrade.IsMaxLevel()) {
+            text.text = maxedOutText;
+        } else {
+            text.text = tempUpgrade.GetCost().ToString();
+        }
+    }
+}
--- Assets/Scripts/Behaviors/UI/TempUpgrade.cs
+++ Assets/Scripts/Behaviors/UI/TempUpgrade.cs
@@ -1,10 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TempUpgrade : MonoBehaviour
 {
+    bool isUnlocked = false;
+
     public TempUpgradeableStat stat;
+
+    public List<TextMeshProUGUI> textToEnable;
+
+    Button selfButton;
+    Image selfImage;
+
+    private void Start() {
+        selfButton = GetComponent<Button>();
+        selfImage = GetComponent<Image>();
+    }
+
+    private void Update() {
+        if (!isUnlocked) {
+            if (IsAvailable()) {
+                isUnlocked = true;
+                selfImage.enabled = true;
+                selfButton.enabled = true;
+                for (int i = 0; i < textToEnable.Count; i++) {
+                    textToEnable[i].enabled = true;
+                }
+            }
+        }
+    }
+
+    public int GetLevel() {
+        return stat.GetCurrentLevel() + 1;
+    }
 
     public int GetCost() {
         return stat.GetCost();
